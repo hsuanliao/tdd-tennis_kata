efa3461d98a31079a5dc4b84182edff8ab0d9e37
Kevin Liao <kevin.liao@mail.aircast.biz>
2022-01-22 17:27:25 +0800

    (Fail test case) A01_Love_All (X-Mind mark 'Processing')

---
+++ TennisTest20220122/TennisTest20220122/TennisGame.cs
@@ -0,0 +1,10 @@
+﻿namespace TennisTest20220122
+{
+    public class TennisGame
+    {
+        public string ScoreResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ TennisTest20220122/TennisTest20220122/TennisTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace TennisTest20220122
+{
+    [TestFixture]
+    public class TennisTest
+    {
+        [Test]
+        public void A01_Love_All()
+        {
+            var tennisGame = new TennisGame();
+            var actual = tennisGame.ScoreResult();
+            actual.Should().Be("Love All");
+        }
+    }
+}
