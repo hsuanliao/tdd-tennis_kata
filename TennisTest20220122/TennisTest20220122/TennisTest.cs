006b27387fad431eaccd320e2e2c82c6b4171b73
Kevin Liao <kevin.liao@mail.aircast.biz>
2022-01-22 17:31:03 +0800

    (Refactor) TennisTest: Extract _tennisGame field and ScoreResultShouldBe() method.

--- TennisTest20220122/TennisTest20220122/TennisTest.cs
+++ TennisTest20220122/TennisTest20220122/TennisTest.cs
@@ -6,12 +6,23 @@ namespace TennisTest20220122
     [TestFixture]
     public class TennisTest
     {
+        private TennisGame _tennisGame;
+
+        [SetUp]
+        public void _00_SetUp()
+        {
+            _tennisGame = new TennisGame();
+        }
+
         [Test]
         public void A01_Love_All()
         {
-            var tennisGame = new TennisGame();
-            var actual = tennisGame.ScoreResult();
-            actual.Should().Be("Love All");
+            ScoreResultShouldBe("Love All");
+        }
+
+        private void ScoreResultShouldBe(string expected)
+        {
+            _tennisGame.ScoreResult().Should().Be(expected);
         }
     }
 }
